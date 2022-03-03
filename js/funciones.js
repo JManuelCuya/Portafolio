5daa193b303e2b7294b127a51d04a6d3c2ec71e1
JManuelCuya <manuelcq2@gmail.com>
2022-03-03 00:46:15 -0500

    3/03

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,17 @@
 $(".menuRespon").on('click', function() {
     $("nav ul").toggle('slow');
 })
+
+(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+        var destino = $(this.hash);
+        if (destino.length == 0) {
+            destino = $('a[name="' + this.hash.substr(1) + '"]');
+        }
+        if (destino.length == 0) {
+            destino = $('html');
+        }
+        $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+        return false;
+    });
+});
